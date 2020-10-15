895b5deec388bc71586df12596c8d6720e450bc0
vinicius-ps <vps.paulo@gmail.com>
2020-10-15 02:02:57 -0300

    add map

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { FiPlus } from 'react-icons/fi';
+import { Map, TileLayer } from 'react-leaflet';
+
+import 'leaflet/dist/leaflet.css';
+
 import mapMarkerImg from '../images/map-marker.svg';
 
 import '../styles/pages/orphanages-map.css';
+
 
 function OrphanagesMaps() {
     return (
@@ -22,7 +27,14 @@ function OrphanagesMaps() {
                 </footer>
             </aside>
 
-            <div></div>
+            <Map
+                center={[-24.0023885, -48.3471341]}
+                zoom={15}
+                style={{ width: '100%', height: '100%' }}
+            >
+                {/* <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" /> */}
+                <TileLayer url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=`} />
+            </Map>
 
             <Link to="/" className="create-orphanage">
                 <FiPlus size={32} color="#FFF" />
